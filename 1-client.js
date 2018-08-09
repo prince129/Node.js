cc932d2aa21a6790e21ecc947ac9879123887ca9
prince129 <39880950+prince129@users.noreply.github.com>
2018-08-09 10:39:11 +0530

    http request through node

---
+++ 1-client.js
@@ -0,0 +1,17 @@
+var http = require('http');
+
+var options = {
+    host: 'www.google.com',
+    port: 80,
+    path: '/',
+    method: 'GET'
+};
+
+console.log("Going to make request...");
+
+var req = http.request(options, function(response){
+    console.log(response.statusCode);
+    response.pipe(process.stdout);
+});
+
+req.end();
